29f92ebd6b4e50bce18aa7fabd4abd627e0d533f
Ryan Hoang <ryanbh1991@gmail.com>
2023-04-11 01:04:36 -0400

    Backbone for boundary subsystem namespace

---
+++ boundary subsystem.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+
+namespace boundary
+{
+    class Form
+    {
+
+    }
+    class StartupMenu: Form
+    {
+
+    }
+    class LoginForm: Form
+    {
+        
+    }
+    class UserMenu: Form
+    {
+        
+    }
+    class UpdateMenu: UserMenu
+    {
+
+    }
+    class addTaskMenu: UserMenu
+    {
+
+    }
+
+
+}
