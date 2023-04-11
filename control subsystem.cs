5f82b78336f7d4172c9ae334f777d1c49f061308
Ryan Hoang <ryanbh1991@gmail.com>
2023-04-11 01:01:58 -0400

    Backbone for control subsystem namespace

---
+++ control subsystem.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+
+namespace control
+{
+    class DBConnector
+    {
+        
+    }
+    class LogoutControl
+    {
+        
+    }
+    class addTaskControl
+    {
+
+    }
+    class StartController
+    {
+
+    }
+    class addTaskControl
+    {
+
+    }
+    class UpdateControl
+    {
+
+    }
+    class LoginControl
+    {
+        
+    }
+}
